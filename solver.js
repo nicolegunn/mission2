980f59f79eb0b27ac6707e4df470b6bb0d203328
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-03-06 12:48:05 +1300

    Solves doubles

--- solver.js
+++ solver.js
@@ -1,7 +1,7 @@
 const SIZE = 9;
 const BOX_SIZE = Math.sqrt(SIZE);
 const sudokuTable = document.getElementById("sudoku-table").firstElementChild; //tbody is child element of table element
-const maximumIterations = 1000000;
+const maximumIterations = 10000;
 
 const rows = {};
 const columns = {};
@@ -198,6 +198,107 @@ function solve() {
     }
   }
 
+  //<----Testing new code below this line------>
+
+  function checkDoubles() {
+    for (box = 1; box <= SIZE; box++) {
+      let idsInPossibleValuesAndBox = Object.keys(possibleValues).filter(
+        (key) => boxes[`box${box}`].includes(Number(key))
+      );
+
+      let length2Keys = idsInPossibleValuesAndBox.filter(
+        (key) => possibleValues[key].length === 2
+      );
+
+      if (length2Keys.length > 0) {
+        for (i = 0; i < length2Keys.length - 1; i++) {
+          for (j = i + 1; j < length2Keys.length; j++) {
+            if (
+              possibleValues[length2Keys[i]].join() ===
+              possibleValues[length2Keys[j]].join()
+            ) {
+              let cell1 = retrieveTableCellInfo(length2Keys[i]);
+              let cell2 = retrieveTableCellInfo(length2Keys[j]);
+
+              for (let id of idsInPossibleValuesAndBox) {
+                if (id !== cell1.ID && id !== cell2.ID) {
+                  let index = possibleValues[id].indexOf(
+                    possibleValues[cell1.ID][0]
+                  );
+
+                  if (index !== -1) {
+                    possibleValues[id].splice(index, 1);
+                  }
+
+                  index = possibleValues[id].indexOf(
+                    possibleValues[cell1.ID][1]
+                  );
+
+                  if (index !== -1) {
+                    possibleValues[id].splice(index, 1);
+                  }
+                }
+              }
+
+              if (cell1.rowClass === cell2.rowClass) {
+                let idsInPossibleValuesAndRow = Object.keys(
+                  possibleValues
+                ).filter((key) => rows[cell1.rowClass].includes(Number(key)));
+
+                for (let id of idsInPossibleValuesAndRow) {
+                  if (id !== cell1.ID && id !== cell2.ID) {
+                    let index = possibleValues[id].indexOf(
+                      possibleValues[cell1.ID][0]
+                    );
+
+                    if (index !== -1) {
+                      possibleValues[id].splice(index, 1);
+                    }
+
+                    index = possibleValues[id].indexOf(
+                      possibleValues[cell1.ID][1]
+                    );
+
+                    if (index !== -1) {
+                      possibleValues[id].splice(index, 1);
+                    }
+                  }
+                }
+              }
+
+              if (cell1.columnClass === cell2.columnClass) {
+                let idsInPossibleValuesAndCol = Object.keys(
+                  possibleValues
+                ).filter((key) =>
+                  columns[cell1.columnClass].includes(Number(key))
+                );
+
+                for (let id of idsInPossibleValuesAndCol) {
+                  if (id !== cell1.ID && id !== cell2.ID) {
+                    let index = possibleValues[id].indexOf(
+                      possibleValues[cell1.ID][0]
+                    );
+                    if (index !== -1) {
+                      possibleValues[id].splice(index, 1);
+                    }
+                    index = possibleValues[id].indexOf(
+                      possibleValues[cell1.ID][1]
+                    );
+                    if (index !== -1) {
+                      possibleValues[id].splice(index, 1);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  //<----Testing new code above this line------>
+
   let length = Object.keys(possibleValues).length;
   let count = 0;
 
@@ -208,7 +309,16 @@ function solve() {
     checkArrays(boxes, "box");
     length = Object.keys(possibleValues).length;
     count++;
-    if (count === maximumIterations) {
+    if (count % 2 === 0) {
+      checkDoubles();
+    }
+    if (count > maximumIterations) {
+      console.log(possibleValues);
+      for (key in possibleValues) {
+        let cell = retrieveTableCellInfo(key);
+        cell.tableCell.value = possibleValues[key];
+        cell.tableCell.style.fontSize = "10px";
+      }
       alert(`Puzzle can not be solved within ${maximumIterations} iterations`);
       break;
     }
@@ -223,6 +333,38 @@ function solvePuzzle(event) {
     alert("Puzzle Invalid");
   } else {
     solve();
+  }
+}
+
+function reset() {
+  for (let i = 1; i <= SIZE * SIZE; i++) {
+    let cell = retrieveTableCellInfo(i);
+    delete enteredValues[String(i)];
+    delete possibleValues[String(i)];
+    cell.tableCell.value = "";
+    cell.tableCell.classList.remove("solved");
+  }
+}
+
+function checkInvalid() {
+  for (let i = 1; i <= SIZE; i++) {
+    let rowArr = rows[`row${i}`];
+    let colArr = columns[`column${i}`];
+    let boxArr = boxes[`box${i}`];
+
+    function checkArr(arr) {
+      let arrVals = [];
+      for (let j = 0; j < SIZE; j++) {
+        let cell = retrieveTableCellInfo(arr[j]);
+        if (cell.value) {
+          arrVals.push(cell.value);
+        }
+      }
+
+      return arrVals.length === new Set(arrVals).size;
+    }
+
+    return !checkArr(rowArr) || !checkArr(colArr) || !checkArr(boxArr);
   }
 }
 
@@ -259,38 +401,38 @@ function populateDemoGame(event) {
   document.getElementById(`cell81`).value = 8;
 }
 
-function reset() {
-  for (let i = 1; i <= SIZE * SIZE; i++) {
-    let cell = retrieveTableCellInfo(i);
-    delete enteredValues[String(i)];
-    delete possibleValues[String(i)];
-    cell.tableCell.value = "";
-    cell.tableCell.classList.remove("solved");
-  }
-}
-
-function checkInvalid() {
-  for (let i = 1; i <= SIZE; i++) {
-    let rowArr = rows[`row${i}`];
-    let colArr = columns[`column${i}`];
-    let boxArr = boxes[`box${i}`];
-
-    function checkArr(arr) {
-      let arrVals = [];
-      for (let j = 0; j < SIZE; j++) {
-        let cell = retrieveTableCellInfo(arr[j]);
-        if (cell.value) {
-          arrVals.push(cell.value);
-        }
-      }
-
-      return arrVals.length === new Set(arrVals).size;
-    }
-
-    return !checkArr(rowArr) || !checkArr(colArr) || !checkArr(boxArr);
-  }
-}
-
-function generatePuzzle() {
+function populateHardDemoGame(event) {
+  event.preventDefault();
   reset();
+  document.getElementById(`cell2`).value = 9;
+  document.getElementById(`cell5`).value = 3;
+  document.getElementById(`cell7`).value = 2;
+  document.getElementById(`cell9`).value = 4;
+  document.getElementById(`cell10`).value = 6;
+  document.getElementById(`cell16`).value = 8;
+  document.getElementById(`cell18`).value = 3;
+  document.getElementById(`cell21`).value = 7;
+  document.getElementById(`cell24`).value = 5;
+  document.getElementById(`cell26`).value = 1;
+  document.getElementById(`cell27`).value = 9;
+  document.getElementById(`cell29`).value = 2;
+  document.getElementById(`cell30`).value = 6;
+  document.getElementById(`cell33`).value = 8;
+  document.getElementById(`cell39`).value = 5;
+  document.getElementById(`cell41`).value = 1;
+  document.getElementById(`cell43`).value = 9;
+  document.getElementById(`cell49`).value = 3;
+  document.getElementById(`cell52`).value = 5;
+  document.getElementById(`cell53`).value = 6;
+  document.getElementById(`cell55`).value = 4;
+  document.getElementById(`cell56`).value = 8;
+  document.getElementById(`cell58`).value = 5;
+  document.getElementById(`cell61`).value = 1;
+  document.getElementById(`cell64`).value = 1;
+  document.getElementById(`cell66`).value = 9;
+  document.getElementById(`cell72`).value = 5;
+  document.getElementById(`cell73`).value = 5;
+  document.getElementById(`cell75`).value = 2;
+  document.getElementById(`cell77`).value = 9;
+  document.getElementById(`cell80`).value = 4;
 }
